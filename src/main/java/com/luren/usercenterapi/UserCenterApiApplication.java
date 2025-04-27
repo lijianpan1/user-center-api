0e17705b7349f3fcf3c788b65b02de2142a2a87e
lijianpan <ljianpan1997@gmail.com>
2025-04-28 00:24:11 +0800

    初始化框架 技术栈：java+spring+springboot+logback+redis+mybatis+mybatis-plus+mysql

---
+++ src/main/java/com/luren/usercenterapi/UserCenterApiApplication.java
@@ -0,0 +1,15 @@
+package com.luren.usercenterapi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.luren.usercenterapi.mapper")
+public class UserCenterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApiApplication.class, args);
+    }
+
+}
